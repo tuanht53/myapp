5df2f258cc2b2d42bb69bcc6d9050b34679634fc
victorkramer <victor.kramer2014@yandex.ru>
2014-01-20 16:12:46 +0400

    Live cursor updating

--- src/com/example/myapp/ContactProvider.java
+++ src/com/example/myapp/ContactProvider.java
@@ -1,16 +1,27 @@
 package com.example.myapp;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
+import android.text.TextUtils;
 import android.util.Log;
 
 public class ContactProvider extends ContentProvider {
 	
 	private static final String TAG = ContactProvider.class.getSimpleName();
 	
+	// Таблица
+	private static final String CONTACT_TABLE = "people";
+	
+	// Поля
+	static final String CONTACT_ID = "_id";
+	static final String CONTACT_NAME = "first_name";
+	static final String CONTACT_PHONE = "phone";
+
 	private static final String AUTHORITY = "com.example.myapp.ContactProvider";
 
 	private static final String CONTACT_PATH = "people";
@@ -18,27 +29,24 @@ public class ContactProvider extends ContentProvider {
 	private static final Uri CONTACT_CONTENT_URI = Uri.parse("content://"
 	      + AUTHORITY + "/" + CONTACT_PATH);
 	
+	// //UriMatcher
+	// общий Uri
+	static final int URI_CONTACTS = 1;
+
+	// Uri с указанным ID
+	static final int URI_CONTACTS_ID = 2;
+
+	// описание и создание UriMatcher
+	private static final UriMatcher uriMatcher;
+	static {
+		uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+		uriMatcher.addURI(AUTHORITY, CONTACT_PATH, URI_CONTACTS);
+		uriMatcher.addURI(AUTHORITY, CONTACT_PATH + "/#", URI_CONTACTS_ID);
+	}
+	
 	private SQLiteDatabase db;
 	private ContactDbHelper dbHelper;
 	
-	@Override
-	public int delete(Uri arg0, String arg1, String[] arg2) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public String getType(Uri uri) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Uri insert(Uri uri, ContentValues values) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
 	@Override
 	public boolean onCreate() {
 		Log.d(TAG, "onCreate");
@@ -48,16 +56,111 @@ public class ContactProvider extends ContentProvider {
 
 	@Override
 	public Cursor query(Uri uri, String[] projection, String selection,
-			String[] selectionArgs, String sortOrder) {
-		// TODO Auto-generated method stub
-		return null;
+		      String[] selectionArgs, String sortOrder) {
+	    Log.d(TAG, "query, " + uri.toString());
+	    // проверяем Uri
+	    switch (uriMatcher.match(uri)) {
+	    case URI_CONTACTS: // общий Uri
+	      Log.d(TAG, "URI_CONTACTS");
+	      // если сортировка не указана, ставим свою - по имени
+	      if (TextUtils.isEmpty(sortOrder)) {
+	        sortOrder = CONTACT_NAME + " ASC";
+	      }
+	      break;
+	    case URI_CONTACTS_ID: // Uri с ID
+	      String id = uri.getLastPathSegment();
+	      Log.d(TAG, "URI_CONTACTS_ID, " + id);
+	      // добавляем ID к условию выборки
+	      if (TextUtils.isEmpty(selection)) {
+	        selection = CONTACT_ID + " = " + id;
+	      } else {
+	        selection = selection + " AND " + CONTACT_ID + " = " + id;
+	      }
+	      break;
+	    default:
+	      throw new IllegalArgumentException("Wrong URI: " + uri);
+	    }
+	    db = dbHelper.getWritableDatabase();
+	    Cursor cursor = db.query(CONTACT_TABLE, projection, selection,
+	        selectionArgs, null, null, sortOrder);
+	    // просим ContentResolver уведомлять этот курсор 
+	    // об изменениях данных в CONTACT_CONTENT_URI
+	    cursor.setNotificationUri(getContext().getContentResolver(),
+	        CONTACT_CONTENT_URI);
+	    return cursor;
+	}
+	
+	@Override
+	public Uri insert(Uri uri, ContentValues values) {
+		Log.d(TAG, "insert, " + uri.toString());
+	    if (uriMatcher.match(uri) != URI_CONTACTS)
+	      throw new IllegalArgumentException("Wrong URI: " + uri);
+
+	    db = dbHelper.getWritableDatabase();
+	    long rowID = db.insert(CONTACT_TABLE, null, values);
+	    Uri resultUri = ContentUris.withAppendedId(CONTACT_CONTENT_URI, rowID);
+	    // уведомляем ContentResolver, что данные по адресу resultUri изменились
+	    getContext().getContentResolver().notifyChange(resultUri, null);
+	    return resultUri;
 	}
 
 	@Override
 	public int update(Uri uri, ContentValues values, String selection,
-			String[] selectionArgs) {
-		// TODO Auto-generated method stub
-		return 0;
+		      String[] selectionArgs) {
+	    Log.d(TAG, "update, " + uri.toString());
+	    switch (uriMatcher.match(uri)) {
+	    case URI_CONTACTS:
+	      Log.d(TAG, "URI_CONTACTS");
+
+	      break;
+	    case URI_CONTACTS_ID:
+	      String id = uri.getLastPathSegment();
+	      Log.d(TAG, "URI_CONTACTS_ID, " + id);
+	      if (TextUtils.isEmpty(selection)) {
+	        selection = CONTACT_ID + " = " + id;
+	      } else {
+	        selection = selection + " AND " + CONTACT_ID + " = " + id;
+	      }
+	      break;
+	    default:
+	      throw new IllegalArgumentException("Wrong URI: " + uri);
+	    }
+	    db = dbHelper.getWritableDatabase();
+	    int cnt = db.update(CONTACT_TABLE, values, selection, selectionArgs);
+	    getContext().getContentResolver().notifyChange(uri, null);
+	    return cnt;
 	}
 
+	@Override
+	public int delete(Uri uri, String selection, String[] selectionArgs) {
+		Log.d(TAG, "delete, " + uri.toString());
+	    switch (uriMatcher.match(uri)) {
+	    case URI_CONTACTS:
+	      Log.d(TAG, "URI_CONTACTS");
+	      break;
+	    case URI_CONTACTS_ID:
+	      String id = uri.getLastPathSegment();
+	      Log.d(TAG, "URI_CONTACTS_ID, " + id);
+	      if (TextUtils.isEmpty(selection)) {
+	        selection = CONTACT_ID + " = " + id;
+	      } else {
+	        selection = selection + " AND " + CONTACT_ID + " = " + id;
+	      }
+	      break;
+	    default:
+	      throw new IllegalArgumentException("Wrong URI: " + uri);
+	    }
+	    db = dbHelper.getWritableDatabase();
+	    int cnt = db.delete(CONTACT_TABLE, selection, selectionArgs);
+	    getContext().getContentResolver().notifyChange(uri, null);
+	    return cnt;
+	}
+
+	@Override
+	public String getType(Uri uri) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
 }
--- src/com/example/myapp/MainActivity.java
+++ src/com/example/myapp/MainActivity.java
@@ -1,21 +1,97 @@
 package com.example.myapp;
 
 import android.app.Activity;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
 	
 	private static final String TAG = MainActivity.class.getSimpleName();
+	
+	// Поля
+	static final String CONTACT_ID = "_id";
+	static final String CONTACT_NAME = "first_name";
+	static final String CONTACT_PHONE = "phone";
+
+	private static final String AUTHORITY = "com.example.myapp.ContactProvider";
+
+	private static final String CONTACT_PATH = "people";
+
+	private static final Uri CONTACT_URI = Uri.parse("content://"
+	      + AUTHORITY + "/" + CONTACT_PATH);
+	
+	SimpleCursorAdapter adapter;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		Log.d(TAG, "OnCreate");
 		setContentView(R.layout.activity_main);
-		SQLiteDatabase db = new ContactDbHelper(this).getReadableDatabase();
+		
+		Cursor cursor = getContentResolver().query(CONTACT_URI, null, null,
+		        null, null);
+		
+		startManagingCursor(cursor);
+		
+		Button addBtn = (Button) findViewById(R.id.add);
+		Button deleteBtn = (Button) findViewById(R.id.delete);
+		Button updateBtn = (Button) findViewById(R.id.update);
+		
+		addBtn.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				ContentValues cv = new ContentValues();
+		        cv.put(CONTACT_NAME, "name 4");
+		        cv.put(CONTACT_PHONE, "email 4");
+		        Uri newUri = getContentResolver().insert(CONTACT_URI, cv);
+		        Log.d(TAG, "insert, result Uri : " + newUri.toString());
+		        adapter.notifyDataSetChanged();
+			}
+		});
+		
+		deleteBtn.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Uri uri = ContentUris.withAppendedId(CONTACT_URI, 3);
+				int cnt = getContentResolver().delete(uri, null, null);
+				Log.d(TAG, "delete, count = " + cnt);
+				adapter.notifyDataSetChanged();
+			}
+		});
+		
+		updateBtn.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				ContentValues cv = new ContentValues();
+		        cv.put(CONTACT_NAME, "name 5");
+		        cv.put(CONTACT_PHONE, "email 5");
+		        Uri uri = ContentUris.withAppendedId(CONTACT_URI, 2);
+		        int cnt = getContentResolver().update(uri, cv, null, null);
+		        Log.d(TAG, "update, count = " + cnt);
+		        adapter.notifyDataSetChanged();
+			}
+		});
+		
+		String from[] = { "first_name", "phone" };
+		int to[] = { android.R.id.text1, android.R.id.text2 };
+		adapter = new SimpleCursorAdapter(this,
+				android.R.layout.simple_list_item_2, cursor, from, to);
+
+		ListView lvContact = (ListView) findViewById(R.id.contactsListView);
+		lvContact.setAdapter(adapter);
+
+		// SQLiteDatabase db = new ContactDbHelper(this).getReadableDatabase();
 		
 	}
 
